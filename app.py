c9da2b16e6967ceef25939dc1b0380c7c3fde10c
Matthew Pryor <mpryor216@gmail.com>
2023-11-15 12:16:14 -0500

    First commit, working demo.

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
+from datetime import datetime
+from bleach import linkify
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable tracking modifications for SQLAlchemy
+app.config['SECRET_KEY'] = 'secret_key'
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
+
+# Define the Message model
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    message = db.Column(db.String(255), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.now, nullable=False)
+
+def create_tables():
+    # Create the database tables
+    db.create_all()
+
+with app.app_context():
+    create_tables()
+
+@app.route('/')
+def index():
+    # Retrieve messages from the database
+    messages = Message.query.all()
+    return render_template('index.html', messages=messages)
+
+@app.route('/send', methods=['POST'])
+def send():
+    username = request.form.get('username')
+    message = request.form.get('message')
+
+    if username and message:
+        # Convert URLs in the message to clickable links
+        message = linkify(message, parse_email=True)
+        # Insert the new message into the database with the current timestamp
+        new_message = Message(username=username, message=message, timestamp=datetime.now())
+        db.session.add(new_message)
+        db.session.commit()
+
+        # Notify clients about the new message
+        socketio.emit('new_message', {'username': username, 'message': message, 'timestamp': new_message.timestamp.strftime('%Y-%m-%d %I:%M %p')})
+
+    return jsonify({'status': 'OK'})
+
+@app.route('/clear_chat', methods=['GET'])
+def clear_chat():
+    # Retrieve the username from the request (you may need to adjust this based on your form structure)
+    username = request.args.get('username', 'Unknown User')
+
+    # Clear all messages from the database
+    Message.query.delete()
+    db.session.commit()
+
+    # Notify clients about the chat being cleared
+    timestamp = datetime.now().strftime('%Y-%m-%d %I:%M %p').strip()
+    socketio.emit('clear_chat', {'username': username, 'timestamp': timestamp})
+
+    # Return a JSON response to indicate the chat has been cleared
+    return jsonify({'status': 'Chat cleared', 'username': username, 'timestamp': timestamp})
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ static/script.js
@@ -0,0 +1,123 @@
+// static/script.js
+$(document).ready(function () {
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+    // new messages
+    socket.on('new_message', function (data) {
+        $('#chat').append('<p><strong>' + data.username + ':</strong> ' + data.message + '<br><span class="timestamp">' + data.timestamp + '</span></p>');
+    });
+
+    // chat clearing
+    socket.on('clear_chat', function (data) {
+        $('#chat').empty();
+        // Display the chat cleared message
+        $('#chat').append('<p class="system-message"><strong>' + data.username + ' cleared the chat at ' + data.timestamp + '</strong></p>');
+    });
+
+    $('#message-form').submit(function (e) {
+        e.preventDefault();
+
+        var username = $('#username').val();
+        var message = $('#message').val();
+
+        $.ajax({
+            type: 'POST',
+            url: '/send',
+            data: { 'username': username, 'message': message },
+            success: function (response) {
+                console.log(response);
+                $('#message').val('');
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
+
+    // Function to handle chat clearing
+    $('#clear-chat').click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'GET',
+            url: '/clear_chat',
+            // Pass the username to the server
+            data: { 'username': $('#username').val() },
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
+
+    // Event listener for changes in the color scheme selector
+    $('#color-scheme').change(function () {
+        // Get the selected color scheme value
+        var selectedColorScheme = $(this).val();
+
+        // Update CSS variables based on the selected color scheme
+        updateColorScheme(selectedColorScheme);
+    });
+
+    // Function to update CSS variables based on the selected color scheme
+    function updateColorScheme(colorScheme) {
+        // Define CSS variable values for each color scheme
+        var colorVariables = {
+            'Ocean Blue': {
+                '--primary-color': '#3498db',
+                '--hover-color': '#2980b9',
+                '--background-color': '#f4f4f4',
+                '--text-color': '#333',
+                '--link-color': 'var(--primary-color)',
+                '--link-hover-color': 'var(--hover-color)',
+                '--system-message-color': '#e74c3c',
+            },
+            'Sunset Red': {
+                '--primary-color': '#e74c3c',
+                '--hover-color': '#c0392b',
+                '--background-color': '#f5e0d8',
+                '--text-color': '#333',
+                '--link-color': 'var(--primary-color)',
+                '--link-hover-color': 'var(--hover-color)',
+                '--system-message-color': '#3498db',
+            },
+            'Forest Green': {
+                '--primary-color': '#2ecc71',
+                '--hover-color': '#27ae60',
+                '--background-color': '#27ae60',  // More intense green background
+                '--text-color': '#333',
+                '--link-color': 'var(--primary-color)',
+                '--link-hover-color': 'var(--hover-color)',
+                '--system-message-color': '#3498db',
+            },
+            'Bubblegum Pink': {
+                '--primary-color': '#e91e63',
+                '--hover-color': '#c2185b',
+                '--background-color': '#f8bbd0',
+                '--text-color': '#333',
+                '--link-color': 'var(--primary-color)',
+                '--link-hover-color': 'var(--hover-color)',
+                '--system-message-color': '#3498db',
+            },
+            'Canary Yellow': {
+                '--primary-color': '#f1c40f',
+                '--hover-color': '#f39c12',
+                '--background-color': '#fcf3cf',
+                '--text-color': '#333',
+                '--link-color': 'var(--primary-color)',
+                '--link-hover-color': 'var(--hover-color)',
+                '--system-message-color': '#3498db',
+            },
+        };
+
+        // Apply the selected color scheme by setting CSS variables
+        var selectedColorSchemeVars = colorVariables[colorScheme];
+        for (var variable in selectedColorSchemeVars) {
+            document.documentElement.style.setProperty(variable, selectedColorSchemeVars[variable]);
+        }
+    }
+
+    // Initialize with the default color scheme
+    updateColorScheme('Ocean Blue');
+});
